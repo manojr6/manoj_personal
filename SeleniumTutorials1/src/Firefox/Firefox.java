15f58f247a72c6c491d1bfc05889330bc5e5c9e6
LIN-51F5VQ3$ <LIN-51F5VQ3$@LIN-51F5VQ3.corp.capgemini.com>
2024-02-06 17:18:43 +0530

    tryy

--- SeleniumTutorials1/src/Firefox/Firefox.java
+++ SeleniumTutorials1/src/Firefox/Firefox.java
@@ -10,7 +10,7 @@ public class Firefox {
 	public static void main(String[] args)  
 	{
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\Downloads\\Selenium\\Firefox driver\\geckodriver-v0.21.0-win64\\geckodriver.exe");
-		WebDriver d11 = new FirefoxDriver();
+		WebDriver d1 = new FirefoxDriver();
 		
 		d1.get("https://ess.kagamierp.com/KagamiAuthService/auth/loginPage#?continue=http:%2F%2Fess.kagamierp.com%2Fkagami-generated_HRMS_GROUP%2Fapi%2Fauth%2Fredirect");
 		d1.findElement(By.name("username")).sendKeys("joseph.t");
